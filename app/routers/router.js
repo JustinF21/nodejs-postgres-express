3107f7dae0303d37b8c1f073c1da18d4f1fd9e9f
JustinF21 <justmarti100@icloud.com>
2024-09-17 11:12:29 -0600

    editRou

--- app/routers/router.js
+++ app/routers/router.js
@@ -5,6 +5,8 @@ let router = express.Router();
  //constasntes de rutas 
 
  const usuarios= require('../controllers/UsuarioController.js');
+ const libros= require('../controllers/LibroController.js');
+ 
  
  
  
@@ -14,6 +16,13 @@ router.get('/api/usuarios/all', usuarios.getAll);
 router.get('/api/usuarios/onebyid/:id_departamento', usuarios.getById);
 router.put('/api/usuarios/update/:id_departamento', usuarios.updateById);
 router.delete('/api/usuarios/delete/:id_departamento', usuarios.deleteById);
+
+router.post('/api/libros/create', libros.create);
+router.get('/api/libros/all', libros.getAll);
+router.get('/api/libros/onebyid/:id_libro', libros.getById);
+router.put('/api/libros/update/:id_libro', libros.updateById);
+router.delete('/api/libros/delete/:id_libro', libros.deleteById);
+
 
 
 
