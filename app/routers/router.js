3c81c60df9d09fbc656282dae3f63a2a46bce956
JustinF21 <justmarti100@icloud.com>
2024-09-25 18:43:36 -0600

    edicionN

--- app/routers/router.js
+++ app/routers/router.js
@@ -1,18 +1,18 @@
 
-let express = require('express');
-let router = express.Router();
-
- //constasntes de rutas 
-
- const usuarios= require('../controllers/UsuarioController.js');
-
-router.post('/usuarios', usuarioController.create);
-router.get('/usuarios', usuarioController.getAll);
-router.get('/usuarios/:id_usuario', usuarioController.getById);
-router.put('/usuarios/:id_usuario', usuarioController.updateById);
-router.delete('/usuarios/:id_usuario', usuarioController.deleteById);
-
-
-
+ const express = require('express');
+ const router = express.Router();
+ 
+ // Importar el controlador
+ const usuarioController = require('../controllers/UsuarioController'); // Verifica que la ruta sea correcta
+ 
+ // Definir las rutas
+ router.post('/usuarios', usuarioController.create);
+ router.get('/usuarios', usuarioController.getAll);
+ router.get('/usuarios/:id_usuario', usuarioController.getById);
+ router.put('/usuarios/:id_usuario', usuarioController.updateById);
+ router.delete('/usuarios/:id_usuario', usuarioController.deleteById);
+ 
+ module.exports = router;
+ 
 module.exports = router;
 
